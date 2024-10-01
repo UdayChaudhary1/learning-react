77f8696eb646f6d91ad3f892e9ef02d83579f6eb
UdayChaudhary1 <114765457+UdayChaudhary1@users.noreply.github.com>
2024-10-01 19:29:43 +0530

    Understanding the react flow

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,9 @@
+import Some from "./some"
+
+function App() {
+  return (
+    <Some/>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/some.jsx
@@ -0,0 +1,7 @@
+function Some(){
+  return (
+    <h3>hello</h3>
+  )
+}
+
+export default Some
